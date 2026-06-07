a060c00fcca69956381747cf782d221896049846
Joao Paulo <jotape.hin@gmail.com>
2026-06-07 15:17:09 -0300

    Cria textos na tela e define a velocidade

--- idle.py
+++ idle.py
@@ -1,5 +1,6 @@
 import pygame, random
 
+# INTERFACE ---
 pygame.init()
 
 altura = 600
@@ -8,17 +9,25 @@ largura = 600
 janela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Aventura Idle!")
 
+fonte = pygame.font.Font(None, 30)
+
+# BOLINHA ---
+cor = (255, 255, 255)
+raio = 20
+
+vida = 10
+velocidade = 0.1
+dano = 1
+
+moedas = 0
+
 # POSIÇÔES ---
 x = 300
 y = 300
 
 # VELOCIDADES ---
-velX = random.uniform(-0.1, 0.1)
-velY = random.uniform(-0.1, 0.1)
-
-# BOLINHA ---
-cor = (255, 255, 255)
-raio = 20
+velX = random.uniform(-velocidade, velocidade)
+velY = random.uniform(-velocidade, velocidade)
 
 rodando = True
 
@@ -26,6 +35,16 @@ while rodando:
     janela.fill((0, 0, 0))
 
     pygame.draw.circle(janela, cor, (x, y), raio)
+
+    textovida = fonte.render(f"Vida: {vida}", True, (255, 255, 255))
+    textovel= fonte.render(f"Velocidade: {velocidade}", True, (255, 255, 255))
+    textodano = fonte.render(f"Dano: {dano}", True, (255, 255, 255))
+    textomoedas = fonte.render(f"Moedas: {moedas}", True, (255, 255, 255))
+
+    janela.blit(textovida, (5, 1))
+    janela.blit(textovel, (5, 25))
+    janela.blit(textodano, (5, 50))
+    janela.blit(textomoedas, (5, 100))
 
     pygame.display.update()
 
