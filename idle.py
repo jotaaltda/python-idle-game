a88eddd3b38ff962e868a9d7e9813db82936bba6
Joao Paulo <jotape.hin@gmail.com>
2026-06-07 15:31:33 -0300

    Cria inimigo

--- idle.py
+++ idle.py
@@ -1,6 +1,6 @@
 import pygame, random
 
-# INTERFACE ---
+# INTERFACE ----
 pygame.init()
 
 altura = 600
@@ -11,7 +11,7 @@ pygame.display.set_caption("Aventura Idle!")
 
 fonte = pygame.font.Font(None, 30)
 
-# BOLINHA ---
+# JOGADOR ----
 cor = (255, 255, 255)
 raio = 20
 
@@ -21,13 +21,27 @@ dano = 1
 
 moedas = 0
 
-# POSIÇÔES ---
 x = 300
 y = 300
 
-# VELOCIDADES ---
 velX = random.uniform(-velocidade, velocidade)
 velY = random.uniform(-velocidade, velocidade)
+# ------------
+
+# INIMIGO ----
+corini = (255, 0, 0)
+raioini = 20
+
+vidaini = 1
+velocidadeini = 0.1
+danoini = 1
+
+xini = random.randint(0, largura)
+yini = random.randint(0, altura)
+
+velXini = random.uniform(-velocidadeini, velocidadeini)
+velYini = random.uniform(-velocidadeini, velocidadeini)
+# ------------
 
 rodando = True
 
@@ -35,16 +49,23 @@ while rodando:
     janela.fill((0, 0, 0))
 
     pygame.draw.circle(janela, cor, (x, y), raio)
+    pygame.draw.circle(janela, corini, (xini, yini), raioini)
 
     textovida = fonte.render(f"Vida: {vida}", True, (255, 255, 255))
     textovel= fonte.render(f"Velocidade: {velocidade}", True, (255, 255, 255))
     textodano = fonte.render(f"Dano: {dano}", True, (255, 255, 255))
     textomoedas = fonte.render(f"Moedas: {moedas}", True, (255, 255, 255))
 
+    textoVidaIni = fonte.render(f"{vidaini}", True, (255, 255, 255))
+
     janela.blit(textovida, (5, 1))
     janela.blit(textovel, (5, 25))
     janela.blit(textodano, (5, 50))
     janela.blit(textomoedas, (5, 100))
+
+    retangulo = textoVidaIni.get_rect(center=(xini, yini))
+
+    janela.blit(textoVidaIni, retangulo)
 
     pygame.display.update()
 
@@ -60,6 +81,15 @@ while rodando:
 
     if y > (altura - raio) or y < raio:
         velY = -velY
+
+    xini += velXini
+    yini += velYini
+
+    if xini > (largura - raioini) or xini < raioini:
+        velXini = -velXini
+
+    if yini > (altura - raioini) or yini < raioini:
+        velYini = -velYini
     
 
 pygame.quit()
